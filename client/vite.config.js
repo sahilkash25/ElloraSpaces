8e88f8116248a3db30b5e754ff58d5ecc0298338
sahilkash25 <sahilkashyap00004@gmai.com>
2026-09-20 21:13:34 +0530

    Configure GitHub Pages deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,14 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
+  base: '/ElloraSpaces/',
 })
